4d9d8f365c44cecec0cae8fd33351a3937c207ff
luciedev166 <lucietriz.dev@gmail.com>
2026-06-29 06:06:36 +0800

    Finished Logic, Control Flow and Filtering

---
+++ Course_02_Intermediate_Python/Chapter_03/03_12.py
@@ -0,0 +1,13 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc = cars['cars_per_cap']
+medium = cars[np.logical_and(cpc < 500, cpc > 100)]
+
+# Print medium
+print(medium)
