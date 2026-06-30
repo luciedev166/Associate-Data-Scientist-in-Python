48218add95949ccdc5c2d516a53cb4a5faa9ef44
luciedev166 <lucietriz.dev@gmail.com>
2026-06-30 10:23:00 +0800

    Finished Chapter_04

---
+++ Course_02_Intermediate_Python/Chapter_04/04_01.py
@@ -0,0 +1,9 @@
+def main():
+    # Initialize offset
+    offset = 8
+
+    # Code the while loop
+    while offset != 0:
+        print("correcting...")
+        offset -= 1
+main()
---
+++ Course_02_Intermediate_Python/Chapter_04/04_08.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for _ in np.nditer(np_height):
+    print(f"{_} inches")
+
+# For loop over np_baseball
+for _ in np.nditer(np_baseball):
+    print(f"{_}")
